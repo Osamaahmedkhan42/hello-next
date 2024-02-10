654b639d252114a2fe3072fe0a760ffd5e819cb4
osamaahmed0042 <osamaahmedkhan0042@gmail.com>
2024-02-10 23:38:27 +0500

    Pagination added

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term)=>{
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -24,6 +25,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
    
   
   return (
+    <>
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
@@ -37,6 +39,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
   defaultValue={searchParams.get('query')?.toString()}
 />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
+    </div></>
   );
 }
